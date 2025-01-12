d5e08eecb20d34c8448ff760b70693d34b82ccf6
willy <willy8742891@gmail.com>
2025-01-12 20:37:29 +0800

    chore: eslint.

---
+++ apps/web-next-app/eslint.config.mjs
@@ -0,0 +1,22 @@
+import path from 'node:path'
+import { fileURLToPath } from 'node:url'
+import js from '@eslint/js'
+import { FlatCompat } from '@eslint/eslintrc'
+import stylistic from '@stylistic/eslint-plugin'
+
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = path.dirname(__filename)
+const compat = new FlatCompat({
+  baseDirectory: __dirname,
+  recommendedConfig: js.configs.recommended,
+  allConfig: js.configs.all,
+})
+export default [
+  ...compat.extends('next/core-web-vitals', 'next/typescript'),
+  stylistic.configs.customize(),
+  {
+    rules: {
+      'import/no-anonymous-default-export': false
+    },
+  },
+]
