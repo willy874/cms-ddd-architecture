b7f8cbe2a6c5a4fbedf4c70477d78462ca3eb5ba
willy <willy8742891@gmail.com>
2025-01-12 23:58:44 +0800

    deploy: docker-compose.

--- apps/api-nest-app/src/main.ts
+++ apps/api-nest-app/src/main.ts
@@ -3,7 +3,7 @@ import { AppModule } from './app.module'
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule)
-  await app.listen(process.env.PORT ?? 3000)
+  await app.listen(process.env.PORT ?? 4000)
   const url = await app.getUrl()
   console.log(`Application is running on: ${url.replace(/\[::1\]/, 'localhost')}/hello`)
 }
