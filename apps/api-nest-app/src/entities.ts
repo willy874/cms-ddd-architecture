36ac364d84bda601d0b3e23ef7a3c27a62102f1c
Willy <front_willy@codingpp.com>
2025-02-19 17:17:03 +0800

    feat: Add Permission and Role entities to entities list

--- apps/api-nest-app/src/entities.ts
+++ apps/api-nest-app/src/entities.ts
@@ -1,4 +1,6 @@
+import { Permission } from './modules/permission/permission.entity'
 import { Product } from './modules/product/product.entity'
+import { Role } from './modules/role/role.entity'
 import { User } from './modules/user/user.entity'
 
-export const entities = [User, Product]
+export const entities = [User, Product, Role, Permission]
