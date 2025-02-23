cf6a3738d9c729708eaf16471968ac250e4d8fcc
willy <willy8742891@gmail.com>
2025-02-23 21:34:31 +0800

    refactor: Add validation for username and password in register and create user DTOs

--- apps/api-nest-app/src/modules/auth/register.dto.ts
+++ apps/api-nest-app/src/modules/auth/register.dto.ts
@@ -1,11 +1,13 @@
-import { IsString, IsNotEmpty } from 'class-validator'
+import { IsString, IsNotEmpty, Matches } from 'class-validator'
 
 export class RegisterDto {
   @IsString()
   @IsNotEmpty()
+  @Matches(/^[a-zA-Z0-9]{4,}$/, { message: 'Username must be at least 4 characters long and contain only letters and numbers.' })
   username: string
 
   @IsString()
   @IsNotEmpty()
+  @Matches(/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)[a-zA-Z\d]{8,}$/, { message: 'Password must contain at least one uppercase letter, one lowercase letter, and one number and must be at least 8 characters long.' })
   password: string
 }
