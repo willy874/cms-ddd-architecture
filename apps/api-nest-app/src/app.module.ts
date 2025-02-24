2fb135531f9639a251cba39612138acac5d8433c
willy <willy8742891@gmail.com>
2025-02-25 01:05:29 +0800

    chore: Add DevtoolsModule for development environment and update dependencies

--- apps/api-nest-app/src/app.module.ts
+++ apps/api-nest-app/src/app.module.ts
@@ -1,4 +1,5 @@
 import { Module } from '@nestjs/common'
+import { DevtoolsModule } from '@nestjs/devtools-integration'
 import { ConfigModule } from '@nestjs/config'
 import { AppController } from './app.controller'
 import { ProductModule } from './modules/product'
@@ -12,6 +13,9 @@ import { RoleModule } from './modules/role/role.module'
 
 @Module({
   imports: [
+    DevtoolsModule.register({
+      http: process.env.NODE_ENV !== 'production',
+    }),
     ConfigModule.forRoot({
       envFilePath: '.env',
       isGlobal: true,
--- apps/api-nest-app/src/main.ts
+++ apps/api-nest-app/src/main.ts
@@ -3,7 +3,10 @@ import { AppModule } from './app.module'
 import { ValidationPipePlugin } from './utils/validation'
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule)
+  const app = await NestFactory.create(AppModule, {
+    snapshot: true,
+  })
+  app.enableShutdownHooks()
   app.setGlobalPrefix(process.env.API_PREFIX || '')
   app.useGlobalPipes(new ValidationPipePlugin())
   await app.listen(process.env.APP_PORT ?? 3000)
