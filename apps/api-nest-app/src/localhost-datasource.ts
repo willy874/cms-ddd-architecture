2c124d436aa71a7e69681a2bc304eb6b329ddf35
willy <willy8742891@gmail.com>
2025-02-25 12:46:49 +0800

    refactor: Update database service command to run migrations

--- apps/api-nest-app/src/localhost-datasource.ts
+++ apps/api-nest-app/src/localhost-datasource.ts
@@ -1,11 +1,10 @@
 import { cwd } from 'node:process'
 import { join } from 'node:path'
 import {} from 'node:fs'
-import dotenv from 'dotenv'
+import { config } from 'dotenv'
 import { DataSource } from 'typeorm'
-import { entities } from './entities'
 
-const env = dotenv.config({ path: join(cwd(), '.env') }).parsed
+const env = config({ path: join(cwd(), '.env') }).parsed
 
 export default new DataSource({
   type: 'mysql',
@@ -14,8 +13,8 @@ export default new DataSource({
   username: env.DATABASE_USER,
   password: env.DATABASE_PASSWORD,
   database: env.DATABASE_NAME,
-  entities: [...entities],
-  migrations: [join(cwd(), 'src/migrations/*.ts')],
+  entities: [join(__dirname, 'entities', '*.ts')],
+  migrations: [join(__dirname, 'migrations', '*.ts')],
   synchronize: false, // 關閉自動同步
   logging: true,
 })
--- apps/api-nest-app/src/migrations/1740371924606-users.ts
+++ apps/api-nest-app/src/migrations/1740371924606-users.ts
@@ -1,34 +1,35 @@
-import { User } from '@/entities/user.entity'
+import { User } from '../entities/user.entity'
 import { MigrationInterface, QueryRunner, Table, EntitySchema, DataSource } from 'typeorm'
 
 export class Users1740371924606 implements MigrationInterface {
   public async up(queryRunner: QueryRunner): Promise<void> {
-    console.log(queryRunner.manager.getRepository(User).metadata.columns)
-    // await queryRunner.createTable(
-    //   new Table({
-    //     name: 'users',
-    //     columns: [
-    //       {
-    //         name: 'id',
-    //         type: 'int',
-    //         isPrimary: true,
-    //         isGenerated: true,
-    //         generationStrategy: 'increment',
-    //       },
-    //       {
-    //         name: 'username',
-    //         type: 'varchar',
-    //         length: '255',
-    //         isUnique: true,
-    //       },
-    //       {
-    //         name: 'password',
-    //         type: 'varchar',
-    //         length: '255',
-    //       },
-    //     ],
-    //   })
-    // )
+    console.log('queryRunner')
+    console.log(queryRunner.manager.getRepository(User).metadata.columns[0].type)
+    await queryRunner.createTable(
+      new Table({
+        name: 'users',
+        columns: [
+          {
+            name: 'id',
+            type: 'int',
+            isPrimary: true,
+            isGenerated: true,
+            generationStrategy: 'increment',
+          },
+          {
+            name: 'username',
+            type: 'varchar',
+            length: '255',
+            isUnique: true,
+          },
+          {
+            name: 'password',
+            type: 'varchar',
+            length: '255',
+          },
+        ],
+      })
+    )
   }
 
   public async down(queryRunner: QueryRunner): Promise<void> {
