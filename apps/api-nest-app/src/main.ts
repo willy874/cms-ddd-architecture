c4af40280c8af22d12602e87cf6165ea0ec9a756
willy <willy8742891@gmail.com>
2025-03-01 20:53:11 +0800

    chore: Update npm dependencies and add RabbitMQ for message queueing

--- apps/api-nest-app/src/main.ts
+++ apps/api-nest-app/src/main.ts
@@ -1,10 +1,20 @@
 import { NestFactory } from '@nestjs/core'
 import { AppModule } from './app.module'
 import { ValidationPipePlugin } from './utils/validation'
+import { MicroserviceOptions, Transport } from '@nestjs/microservices'
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, {
     snapshot: true,
+  })
+  app.connectMicroservice<MicroserviceOptions>({
+    transport: Transport.RMQ,
+    options: {
+      urls: ['amqp://guest:guest@localhost:5672'], // RabbitMQ 連線資訊
+      queueOptions: {
+        durable: false,
+      },
+    },
   })
   app.setGlobalPrefix(process.env.API_PREFIX || '')
   app.useGlobalPipes(new ValidationPipePlugin())
