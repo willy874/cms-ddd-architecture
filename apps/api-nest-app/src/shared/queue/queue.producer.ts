65ed30c2df240dc754859ce432475b08d9d04cd3
willy <willy8742891@gmail.com>
2025-03-02 14:40:10 +0800

    refactor: Update MessageQueueProducer to use Promise for publishing messages

--- apps/api-nest-app/src/shared/queue/queue.producer.ts
+++ apps/api-nest-app/src/shared/queue/queue.producer.ts
@@ -4,13 +4,11 @@ import { MESSAGE_SERVICE } from './queue.module'
 
 @Injectable()
 export class MessageQueueProducer {
-  constructor(@Inject(MESSAGE_SERVICE) private client: ClientProxy) {}
+  constructor(@Inject(MESSAGE_SERVICE) protected client: ClientProxy) {}
 
-  async publish(pattern: string, data: unknown) {
-    return this.client.send(pattern, data).subscribe(
-      (response) => {
-        console.log('Response from RabbitMQ:', response)
-      }
-    )
+  publish(pattern: string, data: unknown) {
+    return new Promise((resolve) => {
+      this.client.send(pattern, data).subscribe(resolve)
+    })
   }
 }
