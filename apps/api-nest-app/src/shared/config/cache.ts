216553592f21a298def7754dd55312becd468c4d
willy <willy8742891@gmail.com>
2025-03-09 08:24:59 +0800

    chore: Update cache configuration to use new environment variable names

--- apps/api-nest-app/src/shared/config/cache.ts
+++ apps/api-nest-app/src/shared/config/cache.ts
@@ -2,6 +2,6 @@ import { registerAs } from '@nestjs/config'
 
 export default registerAs('cache', () => ({
   cacheMode: process.env.CACHE_MODE || 'memory',
-  host: process.env.REDIS_HOST,
-  port: parseInt(process.env.REDIS_PORT, 10),
+  host: process.env.CACHE_HOST,
+  port: parseInt(process.env.CACHE_PORT, 10),
 }))
