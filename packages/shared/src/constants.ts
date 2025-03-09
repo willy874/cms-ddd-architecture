b1043bcdf1b91428585e3e37e4788dd15d43c0a9
willy <willy8742891@gmail.com>
2025-03-09 15:50:43 +0800

    chore: Update shared package with constants and gitignore files

---
+++ packages/shared/src/constants.ts
@@ -0,0 +1,7 @@
+const JWT_SECRET = 'f8pqGisqfvm/DcwjhrG2hFESbNcJbG80NV2Zf92ZY0ZC+9dIaeKsYzqBZXwkHjLpQ2yiEOjrclBeR7NYThz/ow=='
+export const ACCESS_SECRET = 'ACCESS_SECRET' + JWT_SECRET
+export const REFRESH_SECRET = 'REFRESH_SECRET' + JWT_SECRET
+export const QUERY_SECRET = 'QUERY_SECRET' + JWT_SECRET
+export const HASH_SECRET = 'HASH_SECRET' + JWT_SECRET
+export const TOKEN_SECRET = 'TOKEN_SECRET' + JWT_SECRET
+export const TOKEN_TYPE = 'Bearer'
