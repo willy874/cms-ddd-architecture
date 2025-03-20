b06ebff0d76051c1aff1bd37fe9bb7986a27798b
willy <willy8742891@gmail.com>
2025-03-20 10:13:08 +0800

    chore: Update shared constants and environment imports

--- apps/api-service/src/shared/constants.ts
+++ apps/api-service/src/shared/constants.ts
@@ -1,6 +1,1 @@
-const JWT_SECRET = 'f8pqGisqfvm/DcwjhrG2hFESbNcJbG80NV2Zf92ZY0ZC+9dIaeKsYzqBZXwkHjLpQ2yiEOjrclBeR7NYThz/ow=='
-export const ACCESS_SECRET = 'ACCESS_SECRET' + JWT_SECRET
-export const REFRESH_SECRET = 'REFRESH_SECRET' + JWT_SECRET
-export const QUERY_SECRET = 'QUERY_SECRET' + JWT_SECRET
-export const HASH_SECRET = 'HASH_SECRET' + JWT_SECRET
-export const TOKEN_TYPE = 'Bearer'
+export { ACCESS_SECRET, REFRESH_SECRET, QUERY_SECRET, HASH_SECRET, TOKEN_TYPE } from '@packages/shared'
--- packages/shared/src/main.ts
+++ packages/shared/src/main.ts
@@ -1,3 +1,3 @@
-export { ACCESS_SECRET, REFRESH_SECRET, QUERY_SECRET, TOKEN_TYPE } from './constants'
+export { ACCESS_SECRET, REFRESH_SECRET, QUERY_SECRET, HASH_SECRET, TOKEN_TYPE } from './constants'
 export { hash, Jwt } from './crypto'
 export { getEnvironment, loadEnv } from './environment'
