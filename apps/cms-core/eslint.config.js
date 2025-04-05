16e2e0a6637ef6eb087ecd36011a937b65fdaa84
willy <willy8742891@gmail.com>
2025-04-06 02:05:46 +0800

    chore: Refactor React import in localSharedImportMap.js for better compatibility

--- apps/cms-core/eslint.config.js
+++ apps/cms-core/eslint.config.js
@@ -6,7 +6,6 @@ import tseslint from 'typescript-eslint'
 import stylistic from '@stylistic/eslint-plugin'
 
 export default tseslint.config(
-  { ignores: ['dist'] },
   stylistic.configs.customize(),
   {
     extends: [js.configs.recommended, ...tseslint.configs.recommended],
