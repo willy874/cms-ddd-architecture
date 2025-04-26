ed4d783160569c812dc66954a4a7f84d9df1e137
willy <willy8742891@gmail.com>
2025-04-27 01:51:27 +0800

    refactor: Add lazy loading for images in menu components

--- apps/api-service/src/user-service.ts
+++ apps/api-service/src/user-service.ts
@@ -1,19 +1,28 @@
+import { pipe } from 'rxjs'
 import { NestFactory } from '@nestjs/core'
 import { PermissionModule } from './services/permission'
 import { RoleModule } from './services/role'
 import { UserModule } from './services/user'
 import { loadEnv } from '@packages/shared'
+import { initMessageQueueService } from './shared/queue'
+import { MessageQueueModule } from './shared/queue'
+
+const init = pipe(
+  initMessageQueueService(),
+)
 
 async function bootstrap() {
   await loadEnv()
   const app = await NestFactory.create({
     module: class UserAppModule {},
     imports: [
+      MessageQueueModule,
       PermissionModule.register(),
       RoleModule.register(),
       UserModule.register(),
     ],
   })
+  init(app)
   await app.listen(process.env.USER_API_PORT ?? 3000)
 }
 
