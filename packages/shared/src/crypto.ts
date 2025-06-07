6a60064bfd18d9617b6ca9aeab4c7d56fee99ef3
willy <willy8742891@gmail.com>
2025-06-07 15:03:39 +0800

    feat: parse jwt.

--- packages/shared/src/crypto.ts
+++ packages/shared/src/crypto.ts
@@ -2,18 +2,47 @@ import { SHA256 } from "crypto-js"
 import { jwtVerify, SignJWT, errors } from 'jose'
 import { HASH_SECRET } from "./constants"
 
+class VerifyError extends Error {
+  constructor() {
+    super('JWT verification failed')
+    this.name = "VerifyError"
+  }
+}
+
+class ExpiredError extends Error {
+  constructor() {
+    super('JWT has expired')
+    this.name = "ExpiredError"
+  }
+}
+
 export class Jwt {
+  static ExpiredError = ExpiredError
+  static VerifyError = VerifyError
+
   private secret: Uint8Array
   constructor(secret: string) {
     this.secret = new TextEncoder().encode(secret)
   }
 
-  async isExpired(token: string) {
+  async parse(token: string) {
     try {
-      await jwtVerify(token, this.secret)
-      return false
+      const { payload } = await jwtVerify(token, this.secret)
+      return payload
     } catch (error) {
       if (error instanceof errors.JWTExpired) {
+        throw new ExpiredError()
+      }
+      throw new VerifyError()
+    }
+  }
+
+  async isExpired(token: string) {
+    try {
+      await this.parse(token)
+      return false
+    } catch (error) {
+      if (error instanceof ExpiredError) {
         return true
       }
       throw error
@@ -22,7 +51,7 @@ export class Jwt {
 
   async verify(token: string) {
     try {
-      await jwtVerify(token, this.secret)
+      await this.parse(token)
       return true
     } catch (error) {
       return false
