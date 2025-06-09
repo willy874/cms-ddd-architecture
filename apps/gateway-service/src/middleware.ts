d843df34e795ef39e628f95f56920fa9804ac3f1
willy <willy8742891@gmail.com>
2025-06-09 22:01:49 +0800

    refactor: integrate Redis for improved token caching and validation in auth middleware

--- apps/gateway-service/src/middleware.ts
+++ apps/gateway-service/src/middleware.ts
@@ -1,9 +1,14 @@
 import { Jwt } from '@packages/shared';
 import { to } from 'await-to-js';
+import { Redis } from 'ioredis';
 import type { Request, Response, NextFunction } from 'express';
 import { ACCESS_SECRET } from '@packages/shared';
 
 const jwt = new Jwt(ACCESS_SECRET);
+const redisClient = new Redis({
+  host: process.env.CACHE_HOST || 'localhost',
+  port: Number(process.env.CACHE_PORT) || 6379,
+})
 
 export function createAuthMiddleware() {
   return async (req: Request, res: Response, next: NextFunction) => {
@@ -17,12 +22,22 @@ export function createAuthMiddleware() {
       res.status(401).send('Unauthorized');
       return;
     }
-    const [jwtError, isTokenExpired] = await to(jwt.isExpired(token));
-    if (isTokenExpired) {
-      res.status(401).send('Unauthorized');
-      return; 
-    }
+    const [jwtError, jwtPayload] = await to(jwt.parse(token));
     if (jwtError) {
+      res.status(401).send('Unauthorized');
+      return;
+    }
+    if (!jwtPayload || !jwtPayload.jti) {
+      res.status(401).send('Unauthorized');
+      return;
+    }
+    const [cacheError, tokenCacheString] = await to(redisClient.get(jwtPayload.jti));
+    if (cacheError || !tokenCacheString) {
+      res.status(401).send('Unauthorized');
+      return;
+    }
+    const tokenCache = JSON.parse(tokenCacheString);
+    if (!tokenCache || tokenCache.accessToken !== token) {
       res.status(401).send('Unauthorized');
       return;
     }
